4e1ee8354fd43a0270e1895636738ac80617b206
Veronez1 <daviveronez80@gmail.com>
2022-10-25 20:27:09 -0300

    subindo

---
+++ Cliente.cs
@@ -0,0 +1,40 @@
+public class Cliente{
+  
+  // Atributos
+  public long cpf;
+  public string nome;
+  public string endereco;
+
+   // Getters and Setters
+  public void setNome(string value)
+  {
+    nome = value;
+  }
+
+  public string getNome()
+  {
+    return nome;
+  }
+
+  public void setEndereco(string value)
+  {
+    endereco = value;
+  }
+
+  public string getEndereco()
+  {
+    return endereco;
+  }
+
+  public void setCpf(long value)
+  {
+    cpf = value;
+  }
+
+  public long getCpf()
+  {
+    return cpf;
+  }
+
+  
+}
